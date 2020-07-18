b226dee0f09c7ded7fb68dd8828bdd1e43b3dad3
root <root@localhost>
2020-07-18 07:32:22 +0530

    swapping two numbers

--- scanf for a+b.c
+++ scanf for a+b.c
@@ -8,13 +8,10 @@ int main()
 		printf("the numbers u entered are\nA=%d B=%d\n",A, B);	
 		char S[4]="swap";
 		char s[4];
-		printf("printf 'swap' and press enter to swap nos");
+		printf("printf 'swap' and press enter to swap nos\n");
 		gets (s) ;
 		if (s==S) 
-		{
-			B==A;
-			A==C;
-			printf("the swapped numbers are A=%d B=%d" A, B);
+			printf("the swapped numbers are A=%d B=%d", A, B);
 		
 		
 }
