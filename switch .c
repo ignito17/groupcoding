a271d0876c113bcd3a449c4fefba2734c273cba9
root <root@localhost>
2020-07-19 08:54:36 +0530

    changed

--- switch .c
+++ switch .c
@@ -1,17 +1,25 @@
 #include <stdio.h>
-int main()
+   int main()
 {
 	int ID, paswd;
-    printf("please enter ur ID");
-    scanf("%d", &ID) ;
-    
-	 
-
-	
-
-	
-	
-	
-	
-	
+	while (1)
+	{
+    printf("please enter ur ID\n");
+    scanf("%d", & ID);
+    switch (ID) 
+    {
+    case 500:
+    printf("please enter ur password\n" );
+    scanf("%d", & paswd) ;
+    break ;
+    switch (paswd)
+    {
+    	case 123:
+    	printf("Welcome dear user") ;
+    }
+    default :
+    printf("wrong id, please try again\n");
+    }
+	}
+   
 } 
